9a5afe926107508ee8ba1d6efd42ece34d8a31a9
dungvq <dung.vuquoc0102@gmail.com>
2024-04-01 20:36:56 +0700

    update

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -36,51 +36,46 @@
 //     .then((data) => {
 //         document.getElementById("img_3").setAttribute("src", data.responseURL);
 //     });
-"use strict";
-class Car {
-    constructor(name, year) {
-        this._name = name;
-        this._year = year;
-    }
-    hello() {
-        return "This is a car";
-    }
-}
-class Mercedes extends Car {
-    constructor(name, year, model) {
-        super(name, year);
-        this._model = model;
-    }
-    show() {
-        return "This mercedes is " + this.model;
-    }
-    get name() {
-        return this._name;
-    }
-    set name(x) {
-        this._name = x;
-    }
-}
-const mercedes = new Mercedes("Mercedes", 1926, "GLC 300");
-mercedes.name = "Mercedes Electronic";
 
-console.log(mercedes.name);
-
-// $(document).ready(() => {
-//     $(".btnDelete").click(function () {
-//         const userId = $(this).attr("data-id");
-//         // can't not get value of userId
-//         if (confirm("Do you want delete user with id = " + userId + "?")) {
-//             $.get(window.location.href + "delete-user/" + userId, function (data) {
-//                 console.log(data);
-//             });
-//             $(this).closest("tr").remove();
-//         } else {
-//             // Do nothing
-//         }
-//     });
-// });
+// "use strict";
+// class Car {
+//     constructor(name, year) {
+//         this._name = name;
+//         this._year = year;
+//     }
+//     hello() {
+//         return "This is a car";
+//     }
+// }
+// class Mercedes extends Car {
+//     constructor(name, year, model) {
+//         super(name, year);
+//         this._model = model;
+//     }
+//     show() {
+//         return "This mercedes is " + this.model;
+//     }
+//     get name() {
+//         return this._name;
+//     }
+//     set name(x) {
+//         this._name = x;
+//     }
+// }
+// const mercedes = new Mercedes("Mercedes", 1926, "GLC 300");
+// mercedes.name = "Mercedes Electronic";
+// console.log(mercedes.name);
 
 $(document).ready(function () {
-    $(".btnDelete").click(function () {});
+    $(".btnDelete").click(function () {
+        const userId = $(this).attr("data-id");
+        if (confirm("Do you want delete the user with id: " + userId)) {
+            $.get(window.location.href + "delete-user/" + userId, function (data) {
+                console.log(data);
+            });
+            $(this).closest("tr").remove();
+        } else {
+            //do nothing
+        }
+    });
 });
