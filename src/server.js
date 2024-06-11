745283d2972c2ac0f74d844644d0b0697e31a92b
dungvq <dung.vuquoc0102@gmail.com>
2024-06-11 11:13:29 +0700

    mongodb driver

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const webRoutes = require("./routes/web");
 const apiRoutes = require("./routes/api");
 const connection = require("./config/database");
 const fileUpload = require("express-fileupload");
+const { MongoClient } = require("mongodb");
 
 const hostname = process.env.HOST_NAME || "127.0.0.1";
 const port = process.env.PORT || 3001; //1
@@ -20,7 +21,18 @@ app.use("/v1/api/", apiRoutes);
 
 (async () => {
     try {
-        await connection();
+        //mongodb driver
+        const url = process.env.DB_HOST;
+        const client = new MongoClient(url);
+        await client.connect();
+        const dbName = process.env.DB_NAME;
+        const db = client.db(dbName);
+        const collection = db.collection("customers");
+        // collection.insertOne({ name: "dung" });
+        // const result = await collection.find({ name: "dung" }).toArray();
+        // console.log(result);
+        //mongoose
+        // await connection();
         app.listen(port, hostname, () => {
             console.log(`Example app listening on port ${port}`);
         });
